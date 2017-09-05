a3e59ae3dba5d0465be0b44141a000a630988c76
jpfeffer <Net.srfr@gmail.com>
2017-09-05 11:39:54 -0700

    Initial commit

---
+++ bin/task.js
@@ -0,0 +1,20 @@
+'use strict'; // eslint-disable-line
+
+const werelogs = require('werelogs');
+
+const QueueProcessor = require('./PostProcessingConsumer');
+
+const config = require('../../../conf/Config');
+const zkConfig = config.zookeeper;
+const repConfig = config.extensions.replication;
+const sourceConfig = repConfig.source;
+const destConfig = repConfig.destination;
+
+const queueProcessor = new QueueProcessor(zkConfig,
+                                          sourceConfig, destConfig,
+                                          repConfig);
+
+werelogs.configure({ level: config.log.logLevel,
+                     dump: config.log.dumpLevel });
+
+queueProcessor.start();
